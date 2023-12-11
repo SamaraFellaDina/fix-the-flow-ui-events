9341ea6d0cdbe8ced28fc98f1eda6f98e46c5242
SamaraFellaDina <sammybensink@gmail.com>
2023-12-11 11:40:24 +0100

    kutding

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,34 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Animatie 1
+let Frontend = document.querySelector("a:nth-of-type(1)")
+
+Frontend.addEventListener("click", function() {
+Frontend.classList.toggle("active1");
+}); 
+
+// Animatie 2 
+
+let design = document.querySelector("a:nth-of-type(2)")
+let passion = document.querySelector(".passion")
+
+design.addEventListener("click", function() {
+  console.log("passion is my Graphic design😂🫡🤠")
+passion.classList.toggle("passion-show");
+}); 
+
+
+// Animatie 3 
+
+let and = document.querySelector("a:nth-of-type(3)")
+let html = document.querySelector("html")
+
+document.addEventListener("keydown", checkKeyPressed, false);
+
+function checkKeyPressed(e) {
+	if (e.key === 65) {
+		alert("The 'a' key is pressed.");
+	}
+}
